489a0b98a12a55d0d1d9909b2d21faa5f4c362af
Yoseph Yusanto <yosephyusanto@gmail.com>
2025-07-24 00:42:08 +0700

    update migration untuk solve bug status true tapi di SSMS status bernilai 0

---
+++ Migrations/20250723172957_UpdateMsCarSchema.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentCar.Migrations
+{
+    public partial class UpdateMsCarSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

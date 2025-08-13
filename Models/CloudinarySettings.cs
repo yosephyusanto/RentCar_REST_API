cd7007be9fcd168342f9c16058183beb1eac8a72
Yoseph Yusanto <yosephyusanto@gmail.com>
2025-08-13 18:21:54 +0700

    create CloudinarySettings model for depedency injection

---
+++ Models/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace RentCar.Models
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
